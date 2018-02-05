709a9d76ddf0fb13b58c45b560765e9fab606f21
lakhara <siddharth.lakhara@gmail.com>
2018-02-05 13:15:47 +0530

    Combined data -> api1 and 2 -> compromise with setTimout

--- routes/books.js
+++ routes/books.js
@@ -8,41 +8,44 @@ module.exports = [{
 		let dataString = '';
 		let jsonObject = {};
 
+
 		https.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', (responseOuter)=>{
 			responseOuter.setEncoding('utf8');
 			responseOuter.on('data', (data)=>{
 				dataString += data;
 			});
+
 			responseOuter.on('end', ()=>{
 				jsonObject = JSON.parse(dataString);
-
+				let loopVar = 0;
 				for (let books in jsonObject.books){
+					//console.log(books);
+					let ratingString = '';
+					let ratingObject = {};
+					loopVar += 1;
 					let id = jsonObject.books[books].id;
 					https.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/'+id, (responseInner)=>{
-						let ratingString = '';
-						let ratingObject = {};
 						responseInner.setEncoding('utf8');
+
 						responseInner.on('data', (data)=>{
 							ratingString += data;
 						});
+
 						responseInner.on('end', ()=>{
+							//console.log(ratingString);
 							ratingObject = JSON.parse(ratingString);
-							jsonObject.books[books]['rating'] = ratingObject.rating;
-							console.log(jsonObject.books);
+							(jsonObject.books[books])['rating'] = ratingObject.rating;
+							console.log(jsonObject.books); // verifying rating is appended
+							setTimeout(()=>{
+								res({
+									data: jsonObject,
+									statusCode: 200,
+								});
+							}, 4000);
 						});
 					});
-					// jsonObject.books[books]["ratings"] = valueFromAPI2
 				}
-
-				res({
-					data: jsonObject,
-					statusCode: 200,
-				});
-
 			});
 		});
-
-		// https.get();
-
 	}
 }];

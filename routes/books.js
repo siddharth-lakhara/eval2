f02454550002e310f206e33659d2437413887684
lakhara <siddharth.lakhara@gmail.com>
2018-02-05 13:31:39 +0530

    modify for promise

--- routes/books.js
+++ routes/books.js
@@ -18,33 +18,38 @@ module.exports = [{
 			responseOuter.on('end', ()=>{
 				jsonObject = JSON.parse(dataString);
 				let loopVar = 0;
-				for (let books in jsonObject.books){
+				let pr = new Promise((resolve)=>{
+					for (let books in jsonObject.books){
 					//console.log(books);
-					let ratingString = '';
-					let ratingObject = {};
-					loopVar += 1;
-					let id = jsonObject.books[books].id;
-					https.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/'+id, (responseInner)=>{
-						responseInner.setEncoding('utf8');
+						let ratingString = '';
+						let ratingObject = {};
+						loopVar += 1;
+						let id = jsonObject.books[books].id;
+						https.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/'+id, (responseInner)=>{
+							responseInner.setEncoding('utf8');
 
-						responseInner.on('data', (data)=>{
-							ratingString += data;
-						});
+							responseInner.on('data', (data)=>{
+								ratingString += data;
+							});
 
-						responseInner.on('end', ()=>{
-							//console.log(ratingString);
-							ratingObject = JSON.parse(ratingString);
-							(jsonObject.books[books])['rating'] = ratingObject.rating;
-							console.log(jsonObject.books); // verifying rating is appended
-							setTimeout(()=>{
-								res({
-									data: jsonObject,
-									statusCode: 200,
-								});
-							}, 4000);
+							responseInner.on('end', ()=>{
+								//console.log(ratingString);
+								ratingObject = JSON.parse(ratingString);
+								(jsonObject.books[books])['rating'] = ratingObject.rating;
+								console.log(jsonObject.books); // verifying rating is appended
+								if (loopVar === jsonObject.books.length){
+									resolve(jsonObject);
+								}
+							});
 						});
+					}
+				});
+				pr.then(()=>{
+					res({
+						data: jsonObject,
+						statusCode: 200,
 					});
-				}
+				});
 			});
 		});
 	}

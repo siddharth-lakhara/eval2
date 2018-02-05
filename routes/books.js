8ffdc1762ccaba105c0de512299e1303ec90ca96
lakhara <siddharth.lakhara@gmail.com>
2018-02-05 11:45:36 +0530

    Got ratings from api2, verified using console log

--- routes/books.js
+++ routes/books.js
@@ -4,46 +4,44 @@ const https = require('https');
 module.exports = [{
 	method: 'GET',
 	path: '/books',
-	// path: 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks',
 	handler: (req, res)=>{
 		let dataString = '';
 		let jsonObject = {};
-		https.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', (response)=>{
-			response.setEncoding('utf8');
-			response.on('data', (data)=>{
+
+		https.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', (responseOuter)=>{
+			responseOuter.setEncoding('utf8');
+			responseOuter.on('data', (data)=>{
 				dataString += data;
 			});
-			response.on('end', ()=>{
-				// jsonObject = JSON.parse(dataString);
-				// console.log(jsonObject);
+			responseOuter.on('end', ()=>{
+				jsonObject = JSON.parse(dataString);
+
+				for (let books in jsonObject.books){
+					let id = jsonObject.books[books].id;
+					https.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/'+id, (responseInner)=>{
+						let ratingString = '';
+						let ratingObject = {};
+						responseInner.setEncoding('utf8');
+						responseInner.on('data', (data)=>{
+							ratingString += data;
+						});
+						responseInner.on('end', ()=>{
+							ratingObject = JSON.parse(ratingString);
+							console.log(ratingObject);
+						});
+					});
+					// jsonObject.books[books]["ratings"] = valueFromAPI2
+				}
+
 				res({
-					data: dataString,
+					data: jsonObject,
 					statusCode: 200,
 				});
+
 			});
 		});
 
+		// https.get();
+
 	}
 }];
-/*
-    Models.books.findAll().then((result => result.map(row => ({
-			author: row.author,
-			id: row.id,
-			name: row.name
-		}))))
-			.then((bookList) => {
-				res({
-					data: bookList,
-					statusCode: 200,
-				});
-			})
-			.catch((error) => {
-				res({
-					data: `Error in fetching data => ${error}`,
-					statusCode: 500,
-				});
-			});
-
-	},
-}];
-*/
